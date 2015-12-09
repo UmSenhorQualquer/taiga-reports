37004d8292fca33c99263b379b0bca60a682f794
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2015-12-09 00:00:35 +0000

    setup added

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__      = "Ricardo Ribeiro"
+__credits__     = ["Ricardo Ribeiro"]
+__license__     = "MIT"
+__version__     = "0.0"
+__maintainer__  = "Ricardo Ribeiro"
+__email__       = "ricardojvr@gmail.com"
+__status__      = "Development"
+
+
+from setuptools import setup
+
+setup(
+	name				='Taiga stats',
+	version 			='0.0',
+	description 		="""The library provide stats for taiga.io project""",
+	author  			='Ricardo Ribeiro',
+	author_email		='ricardojvr@gmail.com',
+	license 			='MIT',
+	packages			=['taiga_stats',],
+)

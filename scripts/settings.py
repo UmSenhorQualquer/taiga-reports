fac2de5a0804d14632c265edddb668302fc9a9ec
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2015-12-09 01:49:30 +0000

    settings added

---
+++ scripts/settings.py
@@ -0,0 +1,25 @@
+USER = ''
+PASS = ''
+
+TEAM 			= [121, 2323, 434]
+TAGS_PRIORITIES = [
+	('critical', 	1),
+	('urgent', 		2),
+	('important', 	3),
+]
+USER_WORKING_DAYS = {}
+
+
+
+
+
+
+
+
+#holidays = [datetime.datetime(2016, 1, 1,), datetime.datetime(2015, 12, 25,),]
+#r = rrule.rrule(rrule.DAILY,byweekday=[rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR],dtstart=datetime.date.today())
+#rs = rrule.rruleset()
+#rs.rrule(r)
+#for exdate in holidays: rs.exdate(exdate)
+
+#DEFAULT_WORKING_DAYS = rs
